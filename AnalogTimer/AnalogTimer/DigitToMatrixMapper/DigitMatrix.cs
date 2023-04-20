803c7411d345c178cc06b877bcdc30e19e964c9e
unknown <daniilpoyarkov@gmail.com>
2023-04-20 22:29:35 +0300

    Push the branch

---
+++ AnalogTimer/AnalogTimer/DigitToMatrixMapper/DigitMatrix.cs
@@ -0,0 +1,13 @@
+﻿namespace AnalogTimer.DigitToMatrixMapper;
+
+public class DigitMatrix
+{
+    private const int _rows = 7;
+
+    private const int _columns = 8;
+
+    public static void Test()
+    {
+        Console.WriteLine(_rows * _columns);
+    }
+}
