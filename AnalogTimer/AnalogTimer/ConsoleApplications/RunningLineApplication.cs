7388426a7d0f021f2d7b5e89f006e7e9306df97a
Daniil P <daniilpoyarkov@gmail.com>
2023-05-24 16:05:01 +0300

    firsh approach

--- AnalogTimer/AnalogTimer/ConsoleApplications/RunningLineApplication.cs
+++ AnalogTimer/AnalogTimer/ConsoleApplications/RunningLineApplication.cs
@@ -7,16 +7,24 @@ internal class RunningLineApplication : ConsoleApplication
         Console.WriteLine("Write any sentence and watch how it's going!");
     }
 
-    protected override Task HandleUserInput(string? input)
+    protected override async Task HandleUserInput(string? input)
     {
         if (string.IsNullOrEmpty(input))
         {
             Console.WriteLine("Invalid input!");
-            return Task.CompletedTask;
+            return;
         }
 
+        var consoleWidth = Console.BufferWidth - input.Length;
 
-
-        return Task.CompletedTask;
+        while (consoleWidth != 0)
+        {
+            Console.CursorTop = 2;
+            Console.Write(input);
+            consoleWidth--;
+            await Task.Delay(100);
+            Console.CursorLeft = consoleWidth;
+            Console.Write(new string(' ', consoleWidth));
+        }
     }
 }
