43d4ba122436785fd9da52b0796ac535cd615ed3
Daniil P <daniilpoyarkov@gmail.com>
2023-05-24 12:23:44 +0300

    Removed unused method

--- AnalogTimer/AnalogTimer/Program.cs
+++ AnalogTimer/AnalogTimer/Program.cs
@@ -1,32 +1,5 @@
 ﻿using AnalogTimer;
 
-static int LengthOfLastWord(string s)
-{
-    var sum = 0;
-    var wasEmpty = false;
-
-    foreach (var ch in s)
-    {
-        if (ch == ' ')
-        {
-            wasEmpty = true;
-            continue;
-        }
-
-        if (wasEmpty)
-        {
-            wasEmpty = false;
-            sum = 0;
-        }
-
-        sum++;
-    }
-
-    return sum;
-}
-
-var x = LengthOfLastWord("Fly  me   to the moon   ");
-
 var app = new ConsoleApplication();
 
 await app.Run();
