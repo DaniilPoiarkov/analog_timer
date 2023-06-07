e8c7d4545b795d11849b35089c9fdaf2cca0e766
Daniil P <daniilpoyarkov@gmail.com>
2023-06-07 11:24:51 +0300

    Updated matrix display engine

--- AnalogTimer/MatrixDisplayEngine/Contracts/IMatrixDisplay.cs
+++ AnalogTimer/MatrixDisplayEngine/Contracts/IMatrixDisplay.cs
@@ -3,6 +3,4 @@
 public interface IMatrixDisplay
 {
     void Display(List<string> matrix, int horizontalPosition);
-
-    void Display(IEnumerable<List<string>> multipleMatrixes, int horizontalPosition);
 }
